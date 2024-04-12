4df4287d930d00a0520dbb03e932ecdb9efdb474
Axel Andersson <axel.andersson@vyrga.com>
2024-04-12 23:49:52 +0200

    removed eslint extend prettier

--- demo/functions-ts/.eslintrc.js
+++ demo/functions-ts/.eslintrc.js
@@ -11,7 +11,6 @@ module.exports = {
     "plugin:import/typescript",
     "google",
     "plugin:@typescript-eslint/recommended",
-    "prettier"
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
